ed94e9189b5e3ed84c59fa341405da52ab5eb198
autoex <autoex@gmail.com>
2021-07-08 13:21:28 +0300

    add login page css

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import './Login.css'
+import Logo from '../../app/assets/img/LinkedIn.png'
+import Button from "@material-ui/core/Button";
 
 const Login = () => {
+    const register =()=> {}
+    const loginToApp =()=> {}
     return (
-        <div>
-            Login Page
+        <div className='app__login'>
+            <img src={Logo} alt="LinkedIn" className='logo'/>
+            <form action="" className='login__form'>
+                <input type="text" placeholder='Input your name'/>
+                <input type="text" placeholder='Profile pic URL(optional)'/>
+                <input type="email" placeholder='Email'/>
+                <input type="password" placeholder='Password'/>
+                <Button variant="contained" color="primary" type='submit'>
+                    Sing In
+                </Button>
+                <p>Not a member? <span onClick={register} className='login_register'>Register Now</span></p>
+            </form>
         </div>
     );
 };
