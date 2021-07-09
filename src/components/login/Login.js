e7ff2f19bf177483e0c100f0303b2dfca9c1ff40
autoex <autoex@gmail.com>
2021-07-09 11:54:21 +0300

    add register now functionality

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,22 +1,60 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Login.css'
 import Logo from '../../app/assets/img/LinkedIn.png'
 import Button from "@material-ui/core/Button";
+import {auth} from "../../firebaseConf";
+import {useDispatch} from "react-redux";
+import {login} from "../../app/features/userSlice";
+
 
 const Login = () => {
-    const register =()=> {}
-    const loginToApp =()=> {}
+    const [open, setOpen] =useState(false);
+    const [name, setName] = useState();
+    const [email, setEmail] = useState();
+    const [pass, setPass] = useState();
+    const [profilePic, setProfilePic] = useState();
+    const dispatch = useDispatch();
+    const register =()=> {
+        if(!name) { setOpen(true);
+        setTimeout(()=>{setOpen(false)},3000 )}
+
+
+        auth.createUserWithEmailAndPassword(email, pass)
+            .then((userAuth)=> {
+                userAuth.user.updateProfile({
+                    displayName: name,
+                    photoURL: profilePic
+                })
+                    .then(()=> {
+                        dispatch(login({
+                            email: userAuth.user.email,
+                            uid: userAuth.user.uid,
+                            displayName: name,
+                            photoURL: profilePic
+                        }))
+                    })
+            })
+            .catch(err=> console.log(err))
+
+    }
+    const loginToApp =(e)=> {
+        e.preventDefault();
+    }
     return (
         <div className='app__login'>
             <img src={Logo} alt="LinkedIn" className='logo'/>
+            {open && <div className="alert alert-warning" role="alert">
+                Enter a name
+            </div>}
             <form action="" className='login__form'>
-                <input type="text" placeholder='Input your name'/>
-                <input type="text" placeholder='Profile pic URL(optional)'/>
-                <input type="email" placeholder='Email'/>
-                <input type="password" placeholder='Password'/>
-                <Button variant="contained" color="primary" type='submit'>
+                <input type="text" value={name} placeholder='Input your name' onChange={e=>setName(e.target.value)}/>
+                <input type="text" value={profilePic} placeholder='Profile pic URL(optional)'  onChange={e=>setProfilePic(e.target.value)}/>
+                <input type="email" value={email} placeholder='Email' onChange={e=>setEmail(e.target.value)}/>
+                <input type="password" value={pass} placeholder='Password' onChange={e=>setPass(e.target.value)}/>
+                <Button onClick={loginToApp} variant="contained" color="primary" type='submit'>
                     Sing In
                 </Button>
+
                 <p>Not a member? <span onClick={register} className='login_register'>Register Now</span></p>
             </form>
         </div>
