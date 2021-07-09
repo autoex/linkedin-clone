e9a8427b050eb5a3b91973099915bf4602eede0c
autoex <autoex@gmail.com>
2021-07-09 13:18:33 +0300

    stay logined realization

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Header from "./components/header/Header";
 import Sidebar from "./components/sidebar/Sidebar";
 import Feed from "./components/feed/Feed";
 import Widgets from "./widgets/Widgets";
 import './App.css'
-import {useSelector} from "react-redux";
-import {selectUser} from './app/features/userSlice'
+import {useDispatch, useSelector} from "react-redux";
+import {login, logout, selectUser} from './app/features/userSlice'
 import Login from "./components/login/Login";
+import {auth} from "./firebaseConf";
 
 const App = () => {
     const user = useSelector(selectUser);
+    const dispatch = useDispatch();
+    useEffect(()=>{
+        auth.onAuthStateChanged(userAuth => {
+            if (userAuth) {
+            dispatch(login({
+                email: userAuth.email,
+                uid: userAuth.uid,
+                displayName: userAuth.displayName,
+                photoURL: userAuth.photoURL
+            }))
+            }
+            else {
+                dispatch(logout())
+            }
+        })
+    }, [])
 
     return (
         <div className='app'>
